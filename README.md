b27ea5a9bb9d5c0517f18d47cdec9daf348448ee
gustblima <gustavolima320@gmail.com>
2020-03-31 18:19:17 -0300

    Initial code

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # lambda-proxy-requester
 POG to workaround a external service that can only receive requests from our server IP
+
+# Run locally:
+  ```npm run local```
+
+# How to use:
+  Send a request to the API passing your original request on `req` as querystring
+  `ex:localhost:3000?req=api.test.com`
---
+++ src/handler.js
@@ -0,0 +1,34 @@
+const axios = require('axios')
+const proxy = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false
+  const { queryStringParameters, httpMethod, body, headers } = event
+
+  const url = parseQueryStringToUrl(queryStringParameters)
+  console.log(`Requesting URL: ${url}`)
+  try {
+    const response = await axios({
+      method: httpMethod,
+      data: body,
+      url,
+      headers
+    })
+    return buildResponse(response.statusCode, response.data)
+  } catch (e) {
+    console.log(`Error:`, e)
+    return responseBuilder(500, {
+      error: e.code
+    })
+  }
+}
+
+const buildResponse = (statusCode, body) => ({
+  statusCode,
+  body: JSON.stringify(body)
+})
+
+const parseQueryStringToUrl = (queryStrings) => {
+  const { req, ...params } = queryStrings
+  return Object.keys(params).reduce((url, query) => `${url}&${query}=${params[query]}`, req)
+}
+
+module.exports = { proxy }
