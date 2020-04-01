40c56d4a7f0e959c723336ecc67a583b5110fe82
gustblima <gustavolima320@gmail.com>
2020-03-31 21:03:25 -0300

    Add crossdomain and dynamic error status

--- src/handler.js
+++ src/handler.js
@@ -1,21 +1,24 @@
 const axios = require('axios')
+const https = require('https')
 const proxy = async (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false
   const { queryStringParameters, httpMethod, body, headers } = event
 
   const url = parseQueryStringToUrl(queryStringParameters)
-  console.log(`Requesting URL: ${url}`)
+  console.log(`Request ${httpMethod} at URL: ${url}`)
   try {
     const response = await axios({
       method: httpMethod,
       data: body,
-      url,
-      headers
+      url: url,
+      headers,
+      crossdomain: true
     })
     return buildResponse(response.statusCode, response.data)
   } catch (e) {
     console.log(`Error:`, e)
-    return buildResponse(500, {
+    const status = e.status || 500
+    return buildResponse(status, {
       error: e.code
     })
   }
