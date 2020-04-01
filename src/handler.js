fb81520394f728bee09f74466b99286cf083d5ca
gustblima <gustavolima320@gmail.com>
2020-04-01 17:54:09 -0300

    Create methods to modify amazon headers

--- src/handler.js
+++ src/handler.js
@@ -27,4 +27,41 @@ const parseQueryStringToUrl = (queryStrings) => {
   return Object.keys(params).reduce((url, query) => `${url}&${query}=${params[query]}`, req)
 }
 
+const httpRequest = async (url, httpMethod, body, headers) => {
+  return await axios({
+    method: httpMethod,
+    data: body,
+    url: url,
+    headers: {
+      ...removeInvalidHeaders(headers),
+      ["Host"]: getHostName(url)
+    }
+  })
+}
+
+const getHostName = (url) => {
+  const matches = url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i)
+  return matches && matches[1]
+}
+
+const removeInvalidHeaders = (headers) => {
+  const invalidHeaders = [
+    'Host',
+    'User-Agent',
+    'Via',
+    'X-Amz-Cf-Id',
+    'X-Amzn-Trace-Id',
+    'X-Forwarded-For',
+    'X-Forwarded-Port',
+    'X-Forwarded-Proto'
+  ]
+  const newHeader = Object.keys(headers).reduce((obj, key) => {
+    if (!invalidHeaders.includes(key)) {
+      obj[key] = headers[key]
+    }
+    return obj
+  }, {})
+  return newHeader
+}
+
 module.exports = { proxy }
