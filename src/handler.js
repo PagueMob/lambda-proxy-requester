b3a021d22f3193983dd94a61386d89c4bb4501a9
Rafael Duarte Lucchesi <rafael.lucchesi@braspay.co>
2020-04-02 16:07:20 -0300

    Move constants to handler (because I don't know how to use export/require)

--- src/handler.js
+++ src/handler.js
@@ -1,7 +1,25 @@
-import { invalidHeaders, sensitiveHeaders } from './constants'
-
 const axios = require('axios')
 require('axios-debug-log')
+
+const invalidHeaders = [
+  'Host',
+  'User-Agent',
+  'Via',
+  'X-Amz-Cf-Id',
+  'X-Amzn-Trace-Id',
+  'X-Forwarded-For',
+  'X-Forwarded-Port',
+  'X-Forwarded-Proto'
+].map(item => (item.toLowerCase()))
+
+const sensitiveHeaders = [
+  'cookie',
+  'amz',
+  'token',
+  'auth',
+  'secret',
+  'key'
+].map(item => (item.toLowerCase()))
 
 const proxy = async (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false
